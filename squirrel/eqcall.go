bbfdc1a121e07bb591403a3b9663009fee25dd69
Roman Odermatt <roman@eyevip.ch>
2022-02-16 16:52:34 +0100

    add EqCall implements github.com/Masterminds/squirrel.Sqlizer

---
+++ squirrel/eqcall.go
@@ -0,0 +1,13 @@
+package squirrel
+
+import "github.com/Masterminds/squirrel"
+
+type EqCall map[string]func() interface{}
+
+func (eqc *EqCall) ToSql() (sql string, args []interface{}, err error) {
+	eq := squirrel.Eq{}
+	for k, fn := range *eqc {
+		eq[k] = fn()
+	}
+	return eq.ToSql()
+}
