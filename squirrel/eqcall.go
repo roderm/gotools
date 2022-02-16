dca6eccf691d997a58e5e4c42b37ae8ba061c065
Roman Odermatt <roman@eyevip.ch>
2022-02-16 16:59:19 +0100

    no pointer receiver

--- squirrel/eqcall.go
+++ squirrel/eqcall.go
@@ -4,9 +4,9 @@ import "github.com/Masterminds/squirrel"
 
 type EqCall map[string]func() interface{}
 
-func (eqc *EqCall) ToSql() (sql string, args []interface{}, err error) {
+func (eqc EqCall) ToSql() (sql string, args []interface{}, err error) {
 	eq := squirrel.Eq{}
-	for k, fn := range *eqc {
+	for k, fn := range eqc {
 		eq[k] = fn()
 	}
 	return eq.ToSql()
